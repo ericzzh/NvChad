81ca41dd6d5246571ed061e65c00264b45f25759
zhang zhe huan <zzh@zhangs-MacBook-Air.local>
2022-01-12 16:40:23 +0800

    add resize

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -4,6 +4,13 @@ local hooks = require "core.hooks"
 hooks.add("setup_mappings", function(map)
     -- format code
     map("n", "<leader>mf", [[<Cmd> Neoformat<CR>]])
+-- resize with arrows
+-- **** Must set the Option key as Meta key in the xterm preference
+-- **** Or use iterm2
+    map('n', '<M-UP>', '[[<Cmd> resize -2<CR>]]')
+    map('n', '<M-DOWN>', '[[<Cmd> resize +2<CR>]]')
+    map('n', '<M-LEFT>', '[[<Cmd> vertical resize -2<CR>]]')
+    map('n', '<M-RIGHT>', '[[<Cmd> vertical resize +2<CR>]]')
 end)
 
 hooks.add("install_plugins", function(use)
