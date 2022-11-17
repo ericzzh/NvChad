2d22803c400ccb680aa84cedee8f85a626b168ec
zzh <zhangzhehuan1983@126.com>
2022-11-17 14:19:43 +0800

    change

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,36 +1,74 @@
--- This is an example chadrc file , its supposed to be placed in /lua/custom dir
--- lua/custom/chadrc.lua
-
 local M = {}
 
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
+M.mappings = {
+  general = {
+    n = {
+      ["<A-UP>"] = {"<Cmd> resize -2<CR>","resize window up"},
+      ["<A-DOWN>"] = {"<Cmd> resize +2<CR>","resize window down"},
+      ["<A-LEFT>"] = {"<Cmd> vertical resize -2<CR>","resize window left"},
+      ["<A-RIGHT>"] = {"<Cmd> vertical resize +2<CR>","resize window right"},
+    },
+  },
 
+  lspconfig = {
+    n = {
+      ["gd"] = {"<cmd>Telescope lsp_definitions<cr>", "definition list"},
+      ["gi"] = {"<cmd>Telescope lsp_implementations<cr>", "implementation list"},
+      ["gr"] = {"<cmd>Telescope lsp_references<cr>", "code reference list"},
+      ["ge"] = {"<cmd>lua vim.diagnostic.show_line_diagnostics()<CR>", "show line diagnostics"},
+      ["<leader>cr"] = {"<cmd>'<,'> lua vim.lsp.buf.range_code_action()<cr>", "code range actions"},
+      ["<leader>el"] = {"<cmd>Telescope diagnostics<cr>", "Telescope diagnostics"},
+      ["<leader>lo"] = {"<cmd>Telescope lsp_document_symbols<cr>", "Telescope document symbols"},
+      ["<leader>lwo"] = {"<cmd>Telescope lsp_workspace_symbols<cr>", "Telescope workspace symbols"},
+    }
+
+  }
+}
 M.plugins = {
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.lsp-config", -- path of file containing setups of different lsps
-      },
-    nvimtree = {
-         ui = {
-            auto_resize = true,
+
+  ["neovim/nvim-lspconfig"] = {
+      config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.lspconfig"
+      end,
+  },
+
+  -- ["sbdchd/neoformat"] = {
+  --     cmd = "Neoformat"
+  -- },
+
+  ["kyazdani42/nvim-tree.lua"] = {
+      override_options = {
+         view = {
             width = 35,
          },
-      },
+      }
   },
-  user = {
-     ["sbdchd/neoformat"] = {
-       cmd = "Neoformat"
-     }
-  },
+
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+       after = "nvim-lspconfig",
+       config = function()
+          local present, null_ls = pcall(require, "null-ls")
+          if not present then
+             return
+          end
+          local b = null_ls.builtins
+          local sources = {
+             -- webdev stuff
+             -- b.formatting.deno_fmt,
+             b.formatting.prettier,
+             -- Lua
+             -- b.formatting.stylua,
+             -- Shell
+             -- b.formatting.shfmt,
+             -- b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+          }
+          null_ls.setup {
+             debug = true,
+             sources = sources,
+          }
+       end,
+  }
 }
 
-M.options = {
-   -- load your options here or load module with options1
-   user = function() 
-     local opt = vim.opt
-     opt.fileencodings="utf-8,sjis,cp932,default"
-   end,
-
-}
 return M
--- lua/custom/lsp-config.lua
+++ lua/custom/lsp-config.lua
@@ -1,6 +1,8 @@
+-- !!!!!!!!  Deprecated, Just for reference  !!!!!!!
+
 local M={}
 
-M.setup_lsp = function(on_attach, capabilities)
+-- M.setup_lsp = function(on_attach, capabilities)
 
     local function custom_on_attach(_, bufnr)
        local function buf_set_keymap(...)
@@ -15,43 +17,43 @@ M.setup_lsp = function(on_attach, capabilities)
        local opts = { noremap = true, silent = true }
        -- See `:help vim.lsp.*` for documentation on any of the below functions
        -- buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	     -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
        -- buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
+	     -- buf_set_keymap('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
        -- buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	     -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
        -- buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', opts)
+	     buf_set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', opts)
        -- buf_set_keymap("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-        buf_set_keymap("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+       buf_set_keymap("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
        -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+	     -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
        -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+	     -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
        -- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+	     -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
        -- buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+	     -- buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
        -- buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+	     buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
        buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	-- buf_set_keymap('n', '<space>ca', '<cmd>Telescope lsp_code_actions<cr>', opts)
+	     -- buf_set_keymap('n', '<space>ca', '<cmd>Telescope lsp_code_actions<cr>', opts)
        -- buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
-        buf_set_keymap("n", "ge", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+	     buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
+       buf_set_keymap("n", "ge", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
        -- buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-	buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+	     -- buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
        -- buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-	buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-        buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+	     -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+       -- buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
        -- buf_set_keymap("n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
        -- buf_set_keymap("v", "<space>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
-	buf_set_keymap('v', '<space>cr', "<cmd>'<,'> lua vim.lsp.buf.range_code_action()<cr>", opts)
-	buf_set_keymap('n', '<space>es', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-	buf_set_keymap('n', '<space>el', '<cmd>Telescope diagnostics<cr>', opts)
-	buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-	buf_set_keymap("n", "<space>lo", '<cmd>Telescope lsp_document_symbols<cr>', opts)
-	buf_set_keymap("n", "<space>lwo", '<cmd>Telescope lsp_workspace_symbols<cr>', opts)
+	     buf_set_keymap('v', '<space>cr', "<cmd>'<,'> lua vim.lsp.buf.range_code_action()<cr>", opts)
+	     buf_set_keymap('n', '<space>es', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+	     buf_set_keymap('n', '<space>el', '<cmd>Telescope diagnostics<cr>', opts)
+	     buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+	     buf_set_keymap("n", "<space>lo", '<cmd>Telescope lsp_document_symbols<cr>', opts)
+	     buf_set_keymap("n", "<space>lwo", '<cmd>Telescope lsp_workspace_symbols<cr>', opts)
     end
 
     local function setup_servers()
@@ -113,5 +115,6 @@ M.setup_lsp = function(on_attach, capabilities)
         -- update_in_insert = false,
       }
     )
-end
+
+-- end
 return M
